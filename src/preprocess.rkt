341b497b9ba02465b1fd5f047282b80c4e6dd751
Rui Ventura <rui.ventura@tecnico.ulisboa.pt>
2018-05-06 19:37:37 +0100

    Opt for racket instead of racket/base + requires

--- src/preprocess.rkt
+++ src/preprocess.rkt
@@ -19,8 +19,7 @@
 ;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 ;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 ;; SOFTWARE.
-#lang racket/base
-(require racket/contract)
+#lang racket
 
 ; provided macros
 (provide def-active-token)
