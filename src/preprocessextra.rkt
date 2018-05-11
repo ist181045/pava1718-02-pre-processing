ac92eef69a76b58ee9a974585b3d971e5a4fd120
Rui Ventura <rui.ventura@tecnico.ulisboa.pt>
2018-05-11 11:56:02 +0100

    Swap require order in preprocessextra.rkt

--- src/preprocessextra.rkt
+++ src/preprocessextra.rkt
@@ -20,5 +20,5 @@
 ;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 ;; SOFTWARE.
 #lang racket
-(require "preprocess.rkt" "actions.rkt")
+(require "actions.rkt" "preprocess.rkt")
 (add-active-token "#include" include-macro)
